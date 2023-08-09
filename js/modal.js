20d706287911f7e9f99ff4207cc61d53f73d7919
your_space <katerinakaterina182@gmail.com>
2023-08-09 04:23:50 +0300

    modal

--- js/modal.js
+++ js/modal.js
@@ -12,4 +12,19 @@
       refs.modal.classList.toggle("is-hidden");
     }
   })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-location]"),
+      closeModalBtn: document.querySelector("[data-modal-close-location]"),
+      modal: document.querySelector("[data-modal-location]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
   
