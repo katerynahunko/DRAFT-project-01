186ef9443b30e73caa98fdfaca42186542d5f673
your_space <katerinakaterina182@gmail.com>
2023-08-11 06:17:09 +0300

    +

--- js/modal.js
+++ js/modal.js
@@ -1,30 +1,41 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+    modalBackdrop: document.querySelector(".backdrop"),
+    // header: document.querySelector(".header"),
+    body: document.body,
+  };
 
-  (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open-location]"),
-      closeModalBtn: document.querySelector("[data-modal-close-location]"),
-      modal: document.querySelector("[data-modal-location]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
+  refs.openModalBtn.addEventListener("click", openModal);
+  refs.closeModalBtn.addEventListener("click", closeModal);
+  refs.modalBackdrop.addEventListener("click", handleBackdropClick);
+  window.addEventListener("keydown", handleEscPress);
+
+  function openModal() {
+    refs.modal.classList.remove("is-hidden");
+    // refs.header.classList.add("modal-open-header");
+    refs.body.classList.add("modal-open");
+    refs.body.style.overflow = "hidden"; // Disable scrolling
+  }
+
+  function closeModal() {
+    refs.modal.classList.add("is-hidden");
+    // refs.header.classList.remove("modal-open-header");
+    refs.body.classList.remove("modal-open");
+    refs.body.style.overflow = ""; // Enable scrolling
+  }
+
+  function handleBackdropClick(event) {
+    if (event.target === refs.modalBackdrop) {
+      closeModal();
     }
-  })();
-  
+  }
+
+  function handleEscPress(event) {
+    if (event.code === "Escape") {
+      closeModal();
+    }
+  }
+})();
--- js/slider.js
+++ js/slider.js
@@ -1,35 +1,30 @@
-// Отримуємо необхідні елементи з DOM
-const sliderContainer = document.querySelector('.slider-container');
-const dotsContainer = document.querySelector('.slider-dots');
-
-// Отримуємо всі слайди
-const slides = Array.from(document.querySelectorAll('.slide'));
-
-// Створюємо точки відповідно до кількості слайдів
-slides.forEach((_, index) => {
-  const dot = document.createElement('div');
-  dot.classList.add('dot');
+document.addEventListener('DOMContentLoaded', function() {
+  const slider = document.querySelector('.slider');
+  const dotsContainer = document.querySelector('.slider-dots');
+  const slides = Array.from(document.querySelectorAll('.slide'));
   
-  // Налаштовуємо обробник події для переключення слайдів при кліку на точку
-  dot.addEventListener('click', () => {
-    setActiveSlide(index);
+  slides.forEach((_, index) => {
+    const dot = document.createElement('div');
+    dot.classList.add('dot');
+    
+    dot.addEventListener('click', () => {
+      setActiveSlide(index);
+    });
+    
+    dotsContainer.appendChild(dot);
   });
-  
-  dotsContainer.appendChild(dot);
-});
 
-// Функція для встановлення активного слайда та активної точки
-function setActiveSlide(index) {
-  sliderContainer.style.transform = `translateX(-${index * 100}%)`;
-  
-  const activeDot = document.querySelector('.dot.active');
-  if (activeDot) {
-    activeDot.classList.remove('active');
+  function setActiveSlide(index) {
+    slider.style.transform = `translateX(-${index * 100}%)`;
+    
+    const activeDot = document.querySelector('.dot.active');
+    if (activeDot) {
+      activeDot.classList.remove('active');
+    }
+    
+    const dots = Array.from(document.querySelectorAll('.dot'));
+    dots[index].classList.add('active');
   }
   
-  const dots = Array.from(document.querySelectorAll('.dot'));
-  dots[index].classList.add('active');
-}
-
-// Задаємо початковий активний слайд та точку (при потребі)
-setActiveSlide(0);
+  setActiveSlide(0);
+});
